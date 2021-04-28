1c940df43e88326537b17570b341f8ec08e0ff85
Juha Lindqvist <lindqvist.juha@gmail.com>
2021-04-28 14:09:14 +0300

    Exercise 1.02

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM golang:1.16.3-alpine AS builder
 COPY . .
 
 ENV GOPATH=""
-RUN go get -d -v ./...
-RUN go build cmd/service/main.go
+RUN go get -d -v ./... && go build cmd/service/main.go
 
 CMD ["./main"]
--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func randomSequence(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(b)
-}
-
 func main() {
+	fmt.Printf("Server started in port NNNN\n")
 	for {
-		fmt.Printf("%v: %s\n", time.Now().Format(time.RFC3339), randomSequence(10))
-
-		time.Sleep(5 * time.Second)
+		time.Sleep(5 * time.Second) // it's horrible I know
 	}
 }
