6cb70db021763a1573ffd35c0486e8b41ebead78
Juha Lindqvist <lindqvist.juha@gmail.com>
2021-05-04 09:10:57 +0300

    Exercise 1.05

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Server operational!")
+}
+
 func main() {
-	fmt.Printf("Server started in port NNNN\n")
-	for {
-		time.Sleep(5 * time.Second) // it's horrible I know
-	}
+	fmt.Println("Started server, listening on port 3000.")
+	http.HandleFunc("/", homeHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
