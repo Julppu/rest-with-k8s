291e1d42893a954dd4753b7426c2d862793a2bf6
Juha Lindqvist <lindqvist.juha@gmail.com>
2021-05-04 09:25:38 +0300

    Exercise 1.05

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Server operational!")
+	fmt.Fprintf(w, "Server operational: %s!", r.URL.Path[1:])
 }
 
 func main() {
